dc58bac5d26df692a2bbf79f4cc107a018791ad6
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-22 05:08:20 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,24 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material
+  attr_reader :brand
+ 
+  def initialize(brand)
+    @brand = brand
+  end 
+  
+  def condition=(condition)
+    @condition = condition
+  end
+  
+  def condition
+    @condition
+  end
+  
+  def cobble
+    @cobble = cobble.new
+  end
+  
+  def cobble.new
+    @cobble
+  end
+end
